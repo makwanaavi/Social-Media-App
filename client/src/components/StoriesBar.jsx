2b88bb618013eaa8a5c2aae7a2e1670174ca49a1
prabhu-woyce <pparmar@woyce.io>
2025-08-05 17:14:59 +0530

    #update Add storymodal and storetbar section

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { dummyStoriesData } from "../assets/assets";
 import { Plus } from "lucide-react";
+import moment from "moment";
+import StoryModel from "./StoryModel";
 
 const StoriesBar = () => {
   const [stories, setStories] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [viewStory, setViewStory] = useState(null);
+
   const fetchStories = async () => {
     setStories(dummyStoriesData);
   };
@@ -11,11 +16,13 @@ const StoriesBar = () => {
   useEffect(() => {
     fetchStories();
   }, []);
+
   return (
     <div className="w-screen sm:w-[calc(100w-240px)] lg:max-w-2xl no-scrollbar overflow-x-scroll px-4">
       <div className="flex gap-4 pb-5">
         {/*Add Story Card*/}
         <div
+        onClick={() => setShowModal(true)}
           className="rounded-lg shadow-sm min-w-30 max-w-30 max-h-40 aspect-[3/4] cursor-pointer hover:shadow-lg
         transition-all duration-200 border-2 border-dashed border-indigo-300 bg-gradient-to-b from-indigo-50 to-white"
         >
@@ -33,7 +40,7 @@ const StoriesBar = () => {
         {stories.map((story, index) => (
           <div
             key={index}
-            className={`relative rounded-lg shadow min-w-30 max-h-40 cursor-pointer hover:shadow-lg 
+            className={`relative rounded-lg shadow min-w-30 max-h-40 cursor-pointer hover:shadow-lg
           transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}
           >
             <img
@@ -47,11 +54,30 @@ const StoriesBar = () => {
             </p>
 
             <p className="text-white absolute bottom-1 right-2 z-10 text-xs">
-              {story.createdAt}
+              {moment(story.createdAt).fromNow()}
             </p>
+            {story.media_type !== "text" && (
+              <div className="absolute inset-0 z-1 rounded-lg bg-black overflow-hidden">
+                {story.media_type === "image" ? (
+                  <img
+                    src={story.media_url}
+                    alt="story media"
+                    className="w-full h-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80"
+                  />
+                ) : (
+                  <video
+                    src={story.media_url}
+                    className="h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80"
+                  />
+                )}
+              </div>
+            )}
           </div>
         ))}
       </div>
+
+      {/* Add Story Modal*/}
+      { showModal && <StoryModel setShowModal={setShowModal} fetchStories={fetchStories} />}
     </div>
   );
 };
