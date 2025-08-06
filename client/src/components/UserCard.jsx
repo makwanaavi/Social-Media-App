5662b2d5bc67edb8bb6564a56192ac38bf4a741b
prabhu-woyce <pparmar@woyce.io>
2025-08-06 19:20:33 +0530

    #update add usercard

---
+++ client/src/components/UserCard.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+
+import React from "react";
+import { dummyUserData } from "../assets/assets";
+import { MapPin } from "lucide-react";
+
+const UserCard = ({ user }) => {
+  const currentUser = dummyUserData;
+
+  const handleFollow = () => {};
+
+  const handleConnectionRequest = () => {};
+
+  return (
+    <div
+      key={user._id}
+      className="p-4 pt-6 flex flex-col justify-between w-72 shadow border border-gray-200 rounded-md"
+    >
+      <div>
+        <img
+          src={user.profile_picture}
+          alt=""
+          className="rounded-full w-16 shadow-md mx-auto"
+        />
+
+        <p className="mt-4 font-semibold">{user.full_name}</p>
+
+        {user.username && (
+          <p className="text-gray-500 font-light">@{user.username}</p>
+        )}
+        {user.bio && (
+          <p className="text-gray-600 mt-2 text-center text-sm px-4">
+            {user.bio}
+          </p>
+        )}
+      </div>
+
+      <div>
+        <div className="flex items-center gap-1 border border-gray-300 rounded-full px-3 py-1">
+          <MapPin className="w-4 h-4" /> {user.location}
+        </div>
+
+        <div className="flex items-center gap-1 border border-gray-300 rounded-full px-3 py-1">
+          <span>{user.followers.length}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
