0535508378fb9d4ae405baecf9b59e42a96fb0e0
prabhu-woyce <pparmar@woyce.io>
2025-08-06 12:47:59 +0530

    #update Message Section

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import { dummyConnectionsData } from "../assets/assets";
+import { Eye, MessagesSquare } from "lucide-react";
 
 const Messages = () => {
   return (
-    <div>
-      <h1>Messages</h1>
-    </div>
-  )
-}
+    <div className="min-h-screen relative bg-slate-50">
+      <div className="max-w-6xl mx-auto p-6">
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Messages</h1>
+          <p className="text-slate-600">Talk to your friends and family. </p>
+        </div>
 
-export default Messages
+        {/* Connected User */}
+        <div className="flex flex-col gap-3">
+          {dummyConnectionsData.map((user) => (
+            <div
+              key={user._id}
+              className="max-w-xl flex flex-wrap gap-5 p-6 bg-white shadow rounded-md"
+            >
+              <img
+                src={user.profile_picture}
+                alt="profilePic"
+                className="rounded-full size-12 mx-auto"
+              />
+              <div className="flex-1">
+                <p className="font-medium text-slate-700">{user.full_name}</p>
+                <p className="text-slate-500">@{user.username}</p>
+                <p className="text-slate-600 text-sm">{user.bio}</p>
+              </div>
+
+              <div className="flex flex-col gap-1 mt-4">
+                <button
+                  className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 
+                active:scale-95 transition cursor-pointer gap-1"
+                >
+                  <MessagesSquare className="w-4 h-4" />
+                </button>
+
+                <button
+                  className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 
+                active:scale-95 transition cursor-pointer gap-1"
+                >
+                  <Eye className="w-4 h-4" />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
